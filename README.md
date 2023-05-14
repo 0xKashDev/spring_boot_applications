4818676c9398787c585b8f8126ff20d1e8ffef7d
DevDaddy <0xkash22@gmail.com>
2023-05-13 22:30:44 -0400

    updated RestEndpoints.java to inject properties from application.properties

--- README.md
+++ README.md
@@ -3,4 +3,5 @@ Spring Boot Project to help understanding properties
 1. use `https://start.spring.io/` to setup new project
 2. add controllers from previous project `https://github.com/0xKashDev/spring_boot`
 
-3. Updatae controller files and add server port to application.properties
+3. Update controller files and add server port to application.properties
+4. Inject properties in application.properties and RestEndpoints
--- src/main/java/kashdev/understandproperties/controller/RestEndpoints.java
+++ src/main/java/kashdev/understandproperties/controller/RestEndpoints.java
@@ -1,11 +1,23 @@
 package kashdev.understandproperties.controller;
 
 import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Value;
 // `*` takes all annotations
 
 
 @RestController
 public class RestEndpoints {
+
+    //injecting properties
+    @Value("${default.course.name}")
+    private String cName;
+    @Value("${default.course.chapterCount}")
+    private int cchapterCount;
+    @RequestMapping("/defaultCourse")
+    public Course getDefaultCourse(@RequestParam(value="name", defaultValue = "Sprint Boot", required = false) String name,
+                              @RequestParam(value="chapterCount", defaultValue = "2", required = false) int chapterCount){
+        return new Course(cName,cchapterCount);
+    }
 
     @RequestMapping("/course")
     public Course getEndpoint(@RequestParam(value="name", defaultValue = "Sprint Boot", required = false) String name,
